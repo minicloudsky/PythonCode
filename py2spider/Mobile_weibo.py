f3c033b7a0a84de7e5b8da8ce8e1475f97a9f3ce
yawuplus <1397991131@qq.com>
2019-01-17 15:55:44 +0800

    init PyQt5

--- py2spider/Mobile_weibo.py
+++
@@ -1,5 +0,0 @@
-#coding: utf-8
-import requests
-import re
-header = {'User-Agent':'Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3'}
-url = ""
