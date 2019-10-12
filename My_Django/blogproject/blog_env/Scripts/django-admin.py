879040a2d5362d7f1aae4d229bad9155df506cf2
yawuplus <1397991131@qq.com>
2019-10-12 12:32:55 +0800

    remove redundant code

--- My_Django/blogproject/blog_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\software\pycharm\pythoncode\blogproject\blog_env\scripts\python3.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
