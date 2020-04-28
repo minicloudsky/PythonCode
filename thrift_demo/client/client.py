136948b368dd25320eeb57538425ecb5f1c67422
jyw15976 <jiayawu@noahgroup.com>
2020-04-28 17:38:23 +0800

    thirft

--- thrift_demo/client/client.py
+++ thrift_demo/client/client.py
@@ -1,25 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
 import sys
-import jsonfrom test import Transmit
-from test.ttypes import *
-from test.constants import *
+sys.path.append('gen-py')
+from multiplication import MultiplicationService
+from multiplication.ttypes import *
 from thrift import Thrift
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
-
-
-transport = TSocket.TSocket('127.0.0.1', 8000)
-transport = TTransport.TBufferedTransport(transport)
-protocol = TBinaryProtocol.TBinaryProtocol(transport)
-client = Transmit.Client(protocol)
-# Connect!
-transport.open()
-
-cmd = 2
-token = '1111-2222-3333-4444'
-data = json.dumps({"name":"zhoujielun"})
-msg = client.invoke(cmd,token,data)
-print(msg)
-transport.close()
-
-# 执行结果：
+try:
+    # 同样使用 9090 端口，使用阻塞式 I/O 进行传输，是最常见的模式
+    transport = TSocket.TSocket('localhost', 9090)
+    transport = TTransport.TBufferedTransport(transport)
+    # 封装协议，使用二进制编码格式进行传输
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+    # 创建一个 client
+    client = MultiplicationService.Client(protocol)
+    # 打开连接
+    transport.open()
+    n1 = 5
+    n2 = 7
+    product = client.multiply(n1, n2)
+    print '%s * %s = %s' % (n1, n2, product)
+    # 关闭连接
+    transport.close()
+except Thrift.TException, tx:
+    print '%s' % (tx.message)
--- thrift_demo/server/server.py
+++ thrift_demo/server/server.py
@@ -1,37 +1,30 @@
-import json
-from test import Transmit
-from test.ttypes import *
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import sys
+sys.path.append('gen-py')
+from multiplication import MultiplicationService
+from multiplication.ttypes import *
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from thrift.server import TServer
-import socket
-
-
-class TransmitHandler:
-    def __init__(self):
-        self.log = {}
-
-    def sayMsg(self, msg):
-        msg = json.loads(msg)
-        print("sayMsg(" + msg + ")")
-        return "say " + msg + " from " + socket.gethostbyname(socket.gethostname())
-
-    def invoke(self,cmd,token,data):
-        cmd = cmd
-        token =token
-        data = data
-        if cmd ==1:
-            return json.dumps({token:data})
-        else:
-            return 'cmd不匹配'
-
-if __name__=="__main__":
-    handler = TransmitHandler()
-    processor = Transmit.Processor(handler)
-    transport = TSocket.TServerSocket('127.0.0.1', 8000)
+class MultiplicationHandler(object):
+    # multiplication.thrift 文件中已对 `multiploy` 进行了定义，参数为两个整型
+    # 返回值为一整型
+    def multiply(self, n1, n2):
+        print('multify n1 * n2, n1: %s, n2: %s, result: %s' %
+              (n1, n2, n1 * n2))
+        return n1 * n2
+if __name__ == '__main__':
+    handler = MultiplicationHandler()
+    # Processor 用来从连接中读取数据，将处理授权给 handler（自己实现），
+    # 最后将结果写到连接上
+    processor = MultiplicationService.Processor(handler)
+    # 服务端使用 9090 端口， transport 是网络读写抽象层，为到来的连接创建传输对象
+    transport = TSocket.TServerSocket(port=9090)
     tfactory = TTransport.TBufferedTransportFactory()
     pfactory = TBinaryProtocol.TBinaryProtocolFactory()
     server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
-    print("Starting python server...")
+    print('Starting the server...')
     server.serve()
+    print('done.')
